8b8bad1fe843e2633fe826b2cc07a57adb9e6b8e
Paul Copplestone <pcopplestone@gmail.com>
2019-04-07 12:00:27 +0800

    Adding GA

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -4,7 +4,7 @@ module.exports = {
   dest: "public",
   plugins: [
     "@vuepress/last-updated",
-    ["@vuepress/google-analytics", { ga: "" }]
+    ["@vuepress/google-analytics", { ga: "UA-93673521-3" }]
   ],
   themeConfig: {
     nav: [
